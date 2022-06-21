f253bb1f49dbe7de41c62d666ee48a7c9d8aefb5
ChenyanWu <czw390@psu.edu>
2022-06-21 03:01:28 -0400

    q

--- aws_script/lma_train_1.sh
+++ aws_script/lma_train_1.sh
@@ -5,8 +5,7 @@
 # CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
 
 lma_idx=11
-CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/
-tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
+CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
 
 # lma_idx=12
 # CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
