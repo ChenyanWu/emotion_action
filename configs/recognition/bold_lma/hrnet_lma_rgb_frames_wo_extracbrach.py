cb3e6ec8dda8d4eadf1c63d2c69b40acb84b25e9
ChenyanWu <czw390@psu.edu>
2022-06-21 13:12:33 -0400

    .

--- configs/recognition/bold_lma/hrnet_lma_rgb_frames_wo_extracbrach.py
+++ configs/recognition/bold_lma/hrnet_lma_rgb_frames_wo_extracbrach.py
@@ -41,6 +41,7 @@ model = dict(
                 block='BASIC',
                 num_blocks=(4, 4, 4, 4),
                 num_channels=(32, 64, 128, 256))),
+        frozen_stages=4,
         ),
     cls_head=dict(
         type='TSNHead',
