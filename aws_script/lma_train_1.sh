6e17cc8de00b5771ab1a43609318db048c9f3053
ChenyanWu <czw390@psu.edu>
2022-06-21 04:15:43 -0400

    .

--- aws_script/lma_train_1.sh
+++ aws_script/lma_train_1.sh
@@ -1,14 +1,14 @@
-# lma_idx=9
+lma_idx=9
+CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/new_lma_rgb_$lma_idx
+
+lma_idx=10
+CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/new_lma_rgb_$lma_idx
+
+# lma_idx=11
 # CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
 
-# lma_idx=10
-# CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
+lma_idx=12
+CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/new_lma_rgb_$lma_idx
 
-lma_idx=11
-CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
-
-# lma_idx=12
-# CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
-
-# lma_idx=13
-# CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/lma_rgb_$lma_idx
+lma_idx=13
+CUDA_VISIBLE_DEVICES=2,3 PORT=29501 ./tools/dist_train.sh configs/recognition/bold_lma/tsn_r101_1x1x10_100e_lma_rgb_frames_wo_extrabranch.py 2 --validate --deterministic --seed 0 --cfg-options data.train.lma_annot_idx=$lma_idx data.val.lma_annot_idx=$lma_idx data.test.lma_annot_idx=$lma_idx work_dir=./work_dirs/new_lma_rgb_$lma_idx
