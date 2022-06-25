966f5f1e711d1c605f236bed60b18ba9e033defc
ChenyanWu <czw390@psu.edu>
2022-06-25 12:27:01 -0400

    .

--- configs/recognition/bold_lma/hrnet_lma_rgb_frames_wo_extracbrach.py
+++ configs/recognition/bold_lma/hrnet_lma_rgb_frames_wo_extracbrach.py
@@ -168,7 +168,8 @@ optimizer = dict(
     type='Adam',
     lr=5e-4,
 )
-optimizer_config = dict(grad_clip=None)
+# optimizer_config = dict(grad_clip=None)
+optimizer_config = dict(grad_clip=dict(max_norm=40, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
