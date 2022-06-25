ee82ee85ace1db01f9e237d903be9ff5edd06fb7
ChenyanWu <czw390@psu.edu>
2022-06-25 06:41:10 -0400

    .

--- aws_script/init_env.sh
+++ aws_script/init_env.sh
@@ -6,6 +6,10 @@ tar -cvf flow.tar.gz /mnt/efs/fs1/dataset/bold/mmflow
 scp chenyan@data.bridges2.psc.edu:/ocean/projects/iri180005p/chenyan/dataset/emotion_bold/BOLD_public/bold_frame.zip ./
 unzip bold_frame.zip
 
+mkdir annotations && cd annotations
+scp chenyan@data.bridges2.psc.edu:/ocean/projects/iri180005p/chenyan/dataset/emotion_bold/BOLD_public/annotations/*.csv ./
+cd ..
+
 # config environment from scratch (using Amazon Linux Pytorch)
 conda activate pytorch_p39
 pip install git+https://github.com/open-mmlab/mim.git
@@ -15,4 +19,5 @@ cd emotion_action/
 pip install -r requirements/build.txt
 pip install -v -e .
 mkdir -p data/BOLD_public/
-ln -s /mnt/efs/fs1/dataset/bold/* data/BOLD_public/
+ln -s /home/ubuntu/annotations/ data/BOLD_public/
+ln -s /home/ubuntu/mmextract/ data/BOLD_public/
