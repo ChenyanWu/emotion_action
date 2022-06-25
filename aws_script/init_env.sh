06019fb2de1e81926bf4b188d3c366edaa332089
ChenyanWu <czw390@psu.edu>
2022-06-25 06:10:49 -0400

    .

--- aws_script/init_env.sh
+++ aws_script/init_env.sh
@@ -1,17 +1,13 @@
-user@ip-172-31-80-121 emotion_action]$ cd data/BOLD_public/
-(pytorch_p38) [ec2-user@ip-172-31-80-121 BOLD_public]$ ls
-(pytorch_p38) [ec2-user@ip-172-31-80-121 BOLD_public]$ ln -s /mnt/efs/fs1/dataset/bold/* ./
-
-
+ln -s /mnt/efs/fs1/dataset/bold/* ./
 tar -cvf flow.tar.gz /mnt/efs/fs1/dataset/bold/mmflow
 
 
 # scp and extract data
-scp -i /ocean/projects/iri180005p/chenyan/compvis_2.pem flow.tar.gz ec2-user@ec2-44-202-252-202.compute-1.amazonaws.com:/home/ec2-user/
-tar -xvf flow.tar.gz
+scp chenyan@data.bridges2.psc.edu:/ocean/projects/iri180005p/chenyan/dataset/emotion_bold/BOLD_public/bold_frame.zip ./
+unzip bold_frame.zip
 
 # config environment from scratch (using Amazon Linux Pytorch)
-source activate pytorch
+conda activate pytorch_p39
 pip install git+https://github.com/open-mmlab/mim.git
 mim install mmcv-full==1.4.8
 git clone https://github.com/ChenyanWu/emotion_action.git
